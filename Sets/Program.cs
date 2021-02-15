491f9bf6ca8804fd4f5e6a3233a2b0fc2035095c
Andrew Borges <andrewborges71@gmail.com>
2021-02-15 17:28:53 -0300

    Exercicio sobre sets

---
+++ Sets/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> A = new HashSet<int>();
+            HashSet<int> B = new HashSet<int>();
+            HashSet<int> C = new HashSet<int>();
+
+            Console.Write("How many students for course A?");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < n; i++)
+            {
+                int student = int.Parse(Console.ReadLine());
+                A.Add(student);
+            }
+
+            Console.Write("How many students for course A?");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int student = int.Parse(Console.ReadLine());
+                B.Add(student);
+            }
+
+            Console.Write("How many students for course A?");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int student = int.Parse(Console.ReadLine());
+                C.Add(student);
+            }
+
+            HashSet<int> all = new HashSet<int>(A);
+            all.UnionWith(B);
+            all.UnionWith(C);
+            Console.WriteLine($"Total students: {all.Count}");
+        }
+    }
+}
